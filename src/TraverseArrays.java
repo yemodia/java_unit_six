3408ee79a514d35bbe985eb6bdf2b70a80c33a83
diaye <yerim-oumar.dia@ssfs.org>
2020-01-14 15:24:27 -0500

    finished tranverse arrays

--- src/TraverseArrays.java
+++ src/TraverseArrays.java
@@ -20,7 +20,7 @@ public class TraverseArrays {
 
     public static boolean sum28(int[] numbers){
         int xfinal = 0;
-        for (int i = 0; i < numbers.length-1; i++){
+        for (int i = 0; i < numbers.length; i++){
             if (numbers[i] == 2)
                 xfinal += 2;
         }
@@ -33,18 +33,17 @@ public class TraverseArrays {
 
     public static int[] zeroMax(int[] original){
          int [] finalArray = new int[original.length];
-        for (int i = 0; i < original.length-1; i++){
+        for (int i = 0; i < original.length; i++){
             if(original[i] == 0){
                 int max = original[i+1];
                 for (int j = i + 1; j < original.length; j++){
                     if (original[j] % 2 == 1){
-                        if (original[j] > max);
+                        if (original[j] > max)
                         max = original[j];
 
                     }
-                    finalArray[i] = max;
-
                 }
+                finalArray[i] = max;
             }
             else
                 finalArray[i] = original[i];
